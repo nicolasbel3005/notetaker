5487114c783959fb4b33d58232ae6c1eed187fd2
Nicolas Esquibel <n.m.esquibel01@gmail.com>
2023-09-05 22:18:05 -0700

    finished get  request forms/notes

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const fs = require('fs');
-
+const path = require('path');
 const app = express();
-
+app.use(express.static('public'));
 // Serve the notes.html file on the GET /notes route
 app.get('/notes', (req, res) => {
-  res.sendFile(__dirname + '/public/notes.html');
+  res.sendFile(path.join(__dirname , '/public/notes.html'));
 });
+const readDB=()=>{
+    try{
+        return JSON.parse(fs.readFileSync('./db.json'))
+    }catch(e){console.log(e)}
+}
+const writeDB =(data)=>{
+    try{
+        fs.writeFileSync ("./db.json", JSON.stringify(data))
+    }catch(e){console.log(e)}
+}
+app.get("/api/notes",(req, res)=>{
+    const dbDATA = readDB()
+    console.log(dbDATA)
+    res.json(dbDATA)
+}) 
 
 // Serve the index.html file on all other routes
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname , '/public/index.html'));
 });
 
 // Start the server
